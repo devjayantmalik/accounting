0fd3a9c96a051a03afcad77a2ba9c5f8152fa1d4
Jayant Malik <dev.jayantmalik@gmail.com>
2020-05-22 10:12:30 +0530

    initial project setup

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import LockScreen from './src/screens/LockScreen';
+
+const App =  () => {
+  return(
+    <LockScreen />
+  )
+}
+
+export default App;
---
+++ src/screens/LockScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Content, Body, Text } from 'native-base';
+
+const LockScreen = () => {
+  return (
+    <Container>
+      <Body>
+        <Content>
+          <Text>LockScreen</Text>
+        </Content>
+      </Body>
+    </Container>
+  );
+};
+
+export default LockScreen;
