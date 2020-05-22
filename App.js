3ca3251f6f97424868c87d4a695a449eb5ded5c7
Jayant Malik <dev.jayantmalik@gmail.com>
2020-05-22 18:50:51 +0530

    updated index.js and app.js to view screens

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 import LockScreen from './src/screens/LockScreen';
 import Dashboard from './src/screens/Dashboard';
+import Transactions from './src/screens/Transactions';
+import Parties from './src/screens/Parties';
+import Party from './src/screens/Party';
 
 const App = () => {
-  return <Dashboard />;
+  return <LockScreen />;
 };
 
 export default App;
