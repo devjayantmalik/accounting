f37b6d139ef4993845a4f28aef2f71552411ee1e
Jayant Malik <dev.jayantmalik@gmail.com>
2020-05-22 15:20:35 +0530

    fixed outline input dependency error

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React from 'react';
 
 import LockScreen from './src/screens/LockScreen';
+import Dashboard from './src/screens/Dashboard';
 
-const App =  () => {
-  return(
-    <LockScreen />
-  )
-}
+const App = () => {
+  return <Dashboard />;
+};
 
 export default App;
